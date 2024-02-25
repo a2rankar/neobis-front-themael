178b3cef1c38dd9f585f84959d781797e4400b67
Umar <umarsabyrbekov123@gmail.com>
2024-02-25 16:35:48 +0600

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+
+    import React from 'react';
+    import MealOfTheDay from '../components/MealOfTheDay';
+    import SearchPanel from '../components/SearchPanel';
+
+    function Home() {
+      return (
+        <div>
+          <MealOfTheDay />
+          <SearchPanel />
+        </div>
+      );
+    }
+
+    export default Home;
